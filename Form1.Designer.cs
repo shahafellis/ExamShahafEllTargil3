593c9f662cde4916aebc8c9c89359adf136afb80
shahafellis <amziki123@gmail.com>
2020-06-09 22:10:18 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ExamShahafEllTargil3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.downPictureBx = new System.Windows.Forms.PictureBox();
+            this.upPictureBx = new System.Windows.Forms.PictureBox();
+            this.rightPicturebx = new System.Windows.Forms.PictureBox();
+            this.leftPicutreBx = new System.Windows.Forms.PictureBox();
+            this.ballPictureBx = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.downPictureBx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upPictureBx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPicturebx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPicutreBx)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPictureBx)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.ballPictureBx);
+            this.panel1.Location = new System.Drawing.Point(452, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(519, 455);
+            this.panel1.TabIndex = 0;
+            // 
+            // downPictureBx
+            // 
+            this.downPictureBx.Image = global::ExamShahafEllTargil3.Properties.Resources.down;
+            this.downPictureBx.Location = new System.Drawing.Point(210, 249);
+            this.downPictureBx.Name = "downPictureBx";
+            this.downPictureBx.Size = new System.Drawing.Size(100, 71);
+            this.downPictureBx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.downPictureBx.TabIndex = 4;
+            this.downPictureBx.TabStop = false;
+            this.downPictureBx.Click += new System.EventHandler(this.downPictureBx_Click_1);
+            // 
+            // upPictureBx
+            // 
+            this.upPictureBx.Image = global::ExamShahafEllTargil3.Properties.Resources.download;
+            this.upPictureBx.Location = new System.Drawing.Point(210, 114);
+            this.upPictureBx.Name = "upPictureBx";
+            this.upPictureBx.Size = new System.Drawing.Size(100, 65);
+            this.upPictureBx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.upPictureBx.TabIndex = 3;
+            this.upPictureBx.TabStop = false;
+            this.upPictureBx.Click += new System.EventHandler(this.upPictureBx_Click);
+            // 
+            // rightPicturebx
+            // 
+            this.rightPicturebx.Image = global::ExamShahafEllTargil3.Properties.Resources.download1;
+            this.rightPicturebx.Location = new System.Drawing.Point(330, 188);
+            this.rightPicturebx.Name = "rightPicturebx";
+            this.rightPicturebx.Size = new System.Drawing.Size(100, 50);
+            this.rightPicturebx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rightPicturebx.TabIndex = 2;
+            this.rightPicturebx.TabStop = false;
+            this.rightPicturebx.Click += new System.EventHandler(this.rightPicturebx_Click_1);
+            // 
+            // leftPicutreBx
+            // 
+            this.leftPicutreBx.Image = global::ExamShahafEllTargil3.Properties.Resources.down1;
+            this.leftPicutreBx.Location = new System.Drawing.Point(82, 200);
+            this.leftPicutreBx.Name = "leftPicutreBx";
+            this.leftPicutreBx.Size = new System.Drawing.Size(100, 50);
+            this.leftPicutreBx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.leftPicutreBx.TabIndex = 1;
+            this.leftPicutreBx.TabStop = false;
+            this.leftPicutreBx.Click += new System.EventHandler(this.leftPicutreBx_Click);
+            // 
+            // ballPictureBx
+            // 
+            this.ballPictureBx.Image = global::ExamShahafEllTargil3.Properties.Resources.x35859_01_standard;
+            this.ballPictureBx.Location = new System.Drawing.Point(194, 188);
+            this.ballPictureBx.Name = "ballPictureBx";
+            this.ballPictureBx.Size = new System.Drawing.Size(100, 92);
+            this.ballPictureBx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ballPictureBx.TabIndex = 5;
+            this.ballPictureBx.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 450);
+            this.Controls.Add(this.downPictureBx);
+            this.Controls.Add(this.upPictureBx);
+            this.Controls.Add(this.rightPicturebx);
+            this.Controls.Add(this.leftPicutreBx);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.downPictureBx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upPictureBx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPicturebx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPicutreBx)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPictureBx)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox ballPictureBx;
+        private System.Windows.Forms.PictureBox leftPicutreBx;
+        private System.Windows.Forms.PictureBox rightPicturebx;
+        private System.Windows.Forms.PictureBox upPictureBx;
+        private System.Windows.Forms.PictureBox downPictureBx;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamShahafEllTargil3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void upPictureBx_Click(object sender, EventArgs e)
+        {
+            if (ballPictureBx.Location.Y > 3)
+            {
+                ballPictureBx.Location = new Point(ballPictureBx.Location.X, ballPictureBx.Location.Y - 10);
+                Thread.Sleep(50);
+
+            }
+        }
+
+        private void rightPicturebx_Click_1(object sender, EventArgs e)
+        {
+            if (ballPictureBx.Location.X < 420)
+            {
+                ballPictureBx.Location = new Point(ballPictureBx.Location.X + 10, ballPictureBx.Location.Y);
+                Thread.Sleep(50);
+
+
+            }
+        }
+
+        private void downPictureBx_Click_1(object sender, EventArgs e)
+        {
+            if (ballPictureBx.Location.Y < 350)
+            {
+                ballPictureBx.Location = new Point(ballPictureBx.Location.X, ballPictureBx.Location.Y + 10);
+                Thread.Sleep(50);
+            }
+        }
+
+        private void leftPicutreBx_Click(object sender, EventArgs e)
+        {
+            if (ballPictureBx.Location.X > 4)
+            {
+                ballPictureBx.Location = new Point(ballPictureBx.Location.X - 10, ballPictureBx.Location.Y);
+                Thread.Sleep(50);
+
+
+            }
+        }
+    }
+}
+
